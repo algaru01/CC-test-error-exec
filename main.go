3c69cdeee1a74d08d6861be35ef5008f14f37e78
alejandro <alex3galvez@gmail.com>
2024-01-12 20:16:11 +0100

    Added file creation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"example/pkg/arythmetic"
 	"example/pkg/models"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/google/uuid"
@@ -24,6 +25,18 @@ func main() {
 
 	fmt.Println("Result is:", c.N)
 	println("ID is:", id.String())
+
+	nombreArchivo := "ejemplo.txt"
+
+	// Crear o truncar el archivo
+	archivo, err := os.Create(nombreArchivo)
+	if err != nil {
+		fmt.Println("Error al crear el archivo:", err)
+		return
+	}
+	defer archivo.Close()
+
+	fmt.Println("Archivo creado exitosamente:", nombreArchivo)
 
 	time.Sleep(10 * time.Second)
 
